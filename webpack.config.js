f2c5140597e96157cd872b908f100a4782a133c7
Lewis Llobera <billobera@gmail.com>
2020-01-02 18:18:26 +0100

    Remove processing of .js files

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = (env, options) => {
     module: {
       rules: [
         {
-          test: /\.(ts|js)x?$/,
+          test: /\.ts(x)?$/,
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
